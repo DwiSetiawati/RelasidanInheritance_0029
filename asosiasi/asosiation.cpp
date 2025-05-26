db53196e7fd75e2b9409bdaded3a92bed051dbe6
DwiSetiawati <dwisetwa27@gmail.com>
2025-05-26 18:46:59 +0700

    Asosiasi_membuat class pasien

--- asosiasi/asosiation.cpp
+++ asosiasi/asosiation.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+class pasien
+{ // asosiasi
+public:
+    string nama;
+    vector<dokter *> daftar_dokter;
+    pasien(string pNama) : nama(pNama)
+    {
+        cout << "pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien()
+    {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter *);
+    void cetakDokter();
+};
